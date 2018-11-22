897f61d5f88ccfd887fecc4095dca61653a3f8c6
MustafaSnaid2018 <39595514+MustafaSnaid2018@users.noreply.github.com>
2018-11-22 14:09:48 +0200

    MOOD950440TestCase5

---
+++ MOOD-950440_TestCase5.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class MOOD950440TestCase5
+    {
+        private static IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private static string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.katalon.com/";
+        }
+        
+        [ClassCleanup]
+        public static void CleanupClass()
+        {
+            try
+            {
+                //driver.Quit();// quit does not close the window
+                driver.Close();
+                driver.Dispose();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+        
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [TestMethod]
+        public void TheMOOD950440TestCase5Test()
+        {
+            driver.Navigate().GoToUrl("http://localhost/MiTAP.AdminWebSite/TestCase/Edit/950440?ProjectID=1065&GroupName=minerets%20group&GroupID=98346&ProjectName=JinkenTest20");
+            driver.FindElement(By.Id("summary")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | id=summary | ]]
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Summary'])[1]/following::div[2]")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
